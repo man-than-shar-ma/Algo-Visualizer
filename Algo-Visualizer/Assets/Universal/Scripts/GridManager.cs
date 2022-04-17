aafc9488539e8c32fc97f83ee3c14486acf03e2c
Manthan Sharma <72844165+man-than-shar-ma@users.noreply.github.com>
2022-04-17 13:42:37 +0530

    Revert "saving it temporarily"
    This reverts commit 71525bd36853a97e3183472faaaf523c4efb7288.

--- Algo-Visualizer/Assets/Universal/Scripts/GridManager.cs
+++ Algo-Visualizer/Assets/Universal/Scripts/GridManager.cs
@@ -1,28 +1,19 @@
-using UnityEngine;
-using UnityEngine.AI;
 using System.Collections;
 using System.Collections.Generic;
-using NavMeshBuilder = UnityEngine.AI.NavMeshBuilder;
-using Unity.AI.Navigation;
+using UnityEngine;
+using UnityEngine.AI;
 
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private int _width, _height, _extraGrids;
 
     [SerializeField] private Tile _tilePrefab;
-    
-    [SerializeField] private NavMeshSurface surface;
 
-    public GameObject player;
-
+    public 
     // Start is called before the first frame update
     void Start()
     {
         GenerateGrid();
-
-        surface.BuildNavMesh();
-
-        Instantiate(player, Vector3.zero, Quaternion.identity);
     }
 
     // // Update is called once per frame
@@ -38,7 +29,6 @@ public class GridManager : MonoBehaviour
         for(int x=0; x<totalwidth; x++){
             for(int y=0; y<totalheight; y++){
                 var spawnedTile = Instantiate(_tilePrefab, new Vector3(x,0,y), Quaternion.identity);
-                spawnedTile.transform.parent = gameObject.transform;
                 spawnedTile.name = $"Tile {x} {y}";
                 if(x>_extraGrids-1 && x<totalwidth-_extraGrids && y>_extraGrids-1 && y<totalheight-_extraGrids){
                     var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
