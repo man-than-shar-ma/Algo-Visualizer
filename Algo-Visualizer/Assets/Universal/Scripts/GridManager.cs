71525bd36853a97e3183472faaaf523c4efb7288
Manthan Sharma <72844165+man-than-shar-ma@users.noreply.github.com>
2022-04-17 13:10:15 +0530

    saving it temporarily

--- Algo-Visualizer/Assets/Universal/Scripts/GridManager.cs
+++ Algo-Visualizer/Assets/Universal/Scripts/GridManager.cs
@@ -1,19 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+using NavMeshBuilder = UnityEngine.AI.NavMeshBuilder;
+using Unity.AI.Navigation;
 
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private int _width, _height, _extraGrids;
 
     [SerializeField] private Tile _tilePrefab;
+    
+    [SerializeField] private NavMeshSurface surface;
 
-    public 
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
         GenerateGrid();
+
+        surface.BuildNavMesh();
+
+        Instantiate(player, Vector3.zero, Quaternion.identity);
     }
 
     // // Update is called once per frame
@@ -29,6 +38,7 @@ public class GridManager : MonoBehaviour
         for(int x=0; x<totalwidth; x++){
             for(int y=0; y<totalheight; y++){
                 var spawnedTile = Instantiate(_tilePrefab, new Vector3(x,0,y), Quaternion.identity);
+                spawnedTile.transform.parent = gameObject.transform;
                 spawnedTile.name = $"Tile {x} {y}";
                 if(x>_extraGrids-1 && x<totalwidth-_extraGrids && y>_extraGrids-1 && y<totalheight-_extraGrids){
                     var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
